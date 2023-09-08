ad0446e32c49369e1ce65bebd6be942663344309
cyan12green <disissid12@gmail.com>
2023-09-08 09:33:03 +0530

    feat: encrypt protobuf before sending over http

--- simple_exchange_client.cc
+++ simple_exchange_client.cc
@@ -1,4 +1,12 @@
+#include <cstring>
 #include <fstream>
+#include <cstdlib>
+
+
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/wolfcrypt/aes.h>
+#include <wolfssl/wolfcrypt/pkcs7.h>
 
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
@@ -7,6 +15,11 @@
 
 #include "cmake/build/simple_exchange.pb.h"
 
+typedef unsigned char byte;
+typedef unsigned int word32;
+
+const byte KEY[16] = "test";
+
 using simpleexchange::SimpleExample;
 int main(int argc, char** argv) { 
   
@@ -14,10 +27,45 @@ int main(int argc, char** argv) {
   std::string data;
   SimpleExample simple_example;
   simple_example.set_id(74);
-  simple_example.set_text("sample test sent over http"); 
+  simple_example.set_text("sample test sent over http and then a lot of random text to check if this works"); 
   //data contains the protobuf message in a string form
   simple_example.SerializeToString(&data);
+  
+  
+  //encrypting data
+  int length = data.length();
+  int sz;  
+  int padcounter = 0;
+  int i = 0;
 
+  byte* input;
+  byte* output;
+  
+  while (length % AES_BLOCK_SIZE !=0) { 
+    length++;
+    padcounter++;
+  }
+  
+  sz = length * sizeof(byte);
+  input = (byte*) malloc(sz);
+  output = (byte*) malloc(sz);
+
+  std::strcpy((char*) input, data.c_str());
+  
+  for (i = data.length(); i<length; i++) 
+    input[i] = padcounter;
+  
+  Aes enc;
+
+  if ( wc_AesSetKey(&enc, KEY, AES_BLOCK_SIZE, NULL, AES_ENCRYPTION ) != 0)
+    std::cout<<"failed to set key\n";
+  if ( wc_AesCbcEncrypt(&enc, output, input, sz) != 0)
+  /*  std::cout<<"encryption failed\n";
+  else
+    std::cout << "encryption successful\n";
+  */;
+  std::string encryptedData = std::string( reinterpret_cast<char const* >(output), length);
+  std::cout << "encrypted data sent over http: " << encryptedData << std::endl; 
   std::string url = "http://localhost:8080/temp";
 
   try {
@@ -27,9 +75,9 @@ int main(int argc, char** argv) {
   request.setOpt(new curlpp::options::Url(url));
   request.setOpt(new curlpp::options::Verbose(true));
   curlpp::Forms formParts;
-  formParts.push_back(new curlpp::FormParts::Content("protobuf", data));
+  formParts.push_back(new curlpp::FormParts::Content("protobuf", encryptedData));
   request.setOpt(new curlpp::options::HttpPost(formParts)); 
-  std::cout << request << std::endl << request << std::endl;
+  std::cout << request << std::endl;
   return EXIT_SUCCESS;
   
   }
--- simple_exchange_server.cc
+++ simple_exchange_server.cc
@@ -1,15 +1,59 @@
 #include <httpserver.hpp>
 #include <string>
 
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/wolfcrypt/aes.h>
+#include <wolfssl/wolfcrypt/pkcs7.h>
+
 #include "cmake/build/simple_exchange.pb.h"
 using namespace httpserver;
 using simpleexchange::SimpleExample;
 
+typedef unsigned char byte;
+typedef unsigned int word32;
+
+const byte KEY[16] = "test";
+
+
 class test_resource : public http_resource {
   public:
+    
     std::shared_ptr<http_response> render(const http_request& req) {
+      
+      std::string cipher = std::string(req.get_arg("protobuf")); 
+      
+      //decryption
+      Aes dec; 
+      
+      byte* input;
+      byte* output;
+
+      int i = 0;
+      int length = cipher.length();
+      int sz = length * sizeof(byte); 
+      
+      
+      input = (byte*) malloc(sz);
+      output = (byte*) malloc(sz);
+      
+      std::strcpy((char *)input, cipher.c_str());
+      
+      if (wc_AesSetKey(&dec, KEY, AES_BLOCK_SIZE, NULL, AES_DECRYPTION ) != 0)
+        std::cout << "failed to set key\n";
+      
+      if (wc_AesCbcDecrypt(&dec, output, input, sz ) != 0)
+        //std::cout << "decryption failed\n";
+      //else    
+        //std::cout <<"decryption successful\n";
+      ;
+      
+      //getting rid of padding
+      length -= output[length-1];
+      std::string decryptedData = std::string( reinterpret_cast< char const* >(output), length);
+
       SimpleExample simple_example;
-      simple_example.ParseFromString(std::string(req.get_arg("protobuf")));
+      simple_example.ParseFromString(decryptedData);
       return std::shared_ptr<http_response>(new string_response("Protobuf text: " + std::string(simple_example.text())));
     }
 };
