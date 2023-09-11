1d82145736a014ccdcbc647aa1fed5fb81eec9f1
cyan12green <disissid12@gmail.com>
2023-09-11 10:25:06 +0530

    feat: generateResponse and decryptResponse

--- simple_exchange_client.cc
+++ simple_exchange_client.cc
@@ -3,6 +3,9 @@
 #include <cstdlib>
 
 
+#include <iostream>
+#include <memory>
+#include <ostream>
 #include <wolfssl/options.h>
 #include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/wolfcrypt/aes.h>
@@ -19,21 +22,17 @@ typedef unsigned char byte;
 typedef unsigned int word32;
 
 const byte KEY[16] = "test";
+const uint64_t KEY_ID = 56;
 
-using simpleexchange::SimpleExample;
-int main(int argc, char** argv) { 
-  
-  //create the protobuf message
-  std::string data;
-  SimpleExample simple_example;
-  simple_example.set_id(74);
-  simple_example.set_text("sample test sent over http and then a lot of random text to check if this works"); 
-  //data contains the protobuf message in a string form
-  simple_example.SerializeToString(&data);
-  
-  
-  //encrypting data
-  int length = data.length();
+using simpleexchange::ResponseProto;
+
+//populates the protobuf with the encrypted payload
+int generateResponse(const std::string payload, ResponseProto* response_proto ) 
+{ 
+  //TODO: Fetch key from kms
+
+  //encrypting payload
+  int length = payload.length();
   int sz;  
   int padcounter = 0;
   int i = 0;
@@ -47,25 +46,88 @@ int main(int argc, char** argv) {
   }
   
   sz = length * sizeof(byte);
-  input = (byte*) malloc(sz);
+  input  = (byte*) malloc(sz);
   output = (byte*) malloc(sz);
 
-  std::strcpy((char*) input, data.c_str());
+  std::memcpy((char*) input, payload.c_str(), sz);
   
-  for (i = data.length(); i<length; i++) 
+  for (i = payload.length(); i<length; i++) 
     input[i] = padcounter;
   
   Aes enc;
 
   if ( wc_AesSetKey(&enc, KEY, AES_BLOCK_SIZE, NULL, AES_ENCRYPTION ) != 0)
-    std::cout<<"failed to set key\n";
+    return EXIT_FAILURE; //std::cout<<"failed to set key\n";
   if ( wc_AesCbcEncrypt(&enc, output, input, sz) != 0)
-  /*  std::cout<<"encryption failed\n";
-  else
-    std::cout << "encryption successful\n";
-  */;
-  std::string encryptedData = std::string( reinterpret_cast<char const* >(output), length);
-  std::cout << "encrypted data sent over http: " << encryptedData << std::endl; 
+    return EXIT_FAILURE; //std::cout<<"encryption failed\n";
+  
+  std::string encrypted_payload = std::string( reinterpret_cast<char const* >(output), length);
+  
+  response_proto->set_encrypted_payload(encrypted_payload);
+  response_proto->set_key_number(KEY_ID);
+  
+  std::cout << "Encryption successful\n";
+  return EXIT_SUCCESS;
+}
+
+//decrypts the payload contained in protobuf and outputs a string
+std::string decryptResponse(const ResponseProto response_proto) 
+{ 
+  //TODO get key
+  
+  
+  std::string cipher = std::string(response_proto.encrypted_payload());
+  //decryption
+  Aes dec; 
+
+  byte* input;
+  byte* output;
+
+  int i = 0;
+  int length = cipher.length();
+  int sz = length * sizeof(byte); 
+
+
+  input = (byte*) malloc(sz);
+  output = (byte*) malloc(sz);
+
+  std::memcpy((char *)input, cipher.c_str(), sz);
+  
+  if (wc_AesSetKey(&dec, KEY, AES_BLOCK_SIZE, NULL, AES_DECRYPTION ) != 0)
+    std::cout << "failed to set key\n";
+
+  if (wc_AesCbcDecrypt(&dec, output, input, sz ) != 0) 
+  { 
+    std::cout << "decryption failed\n";
+    return NULL;
+  }
+  
+  //getting rid of padding
+  length -= output[length-1];
+
+  std::cout << "decryption successful\n";
+  return std::string( reinterpret_cast< char const* >(output), length); 
+}
+
+int main(int argc, char** argv) { 
+  
+  //create the protobuf message
+  std::string data, payload;
+  ResponseProto response_proto, temp;
+  if (generateResponse("asset/url/media.mpeg", &response_proto) != 0)
+    return EXIT_FAILURE;
+  
+  //data contains the protobuf message in a string form
+  response_proto.SerializeToString(&data);
+  
+  //temp is initialized
+  temp.ParseFromString(data);
+  
+  payload = decryptResponse(temp);
+  std::cout << payload <<std::endl;  
+  
+
+  //Sending over http
   std::string url = "http://localhost:8080/temp";
 
   try {
@@ -75,7 +137,7 @@ int main(int argc, char** argv) {
   request.setOpt(new curlpp::options::Url(url));
   request.setOpt(new curlpp::options::Verbose(true));
   curlpp::Forms formParts;
-  formParts.push_back(new curlpp::FormParts::Content("protobuf", encryptedData));
+  formParts.push_back(new curlpp::FormParts::Content("protobuf", data));
   request.setOpt(new curlpp::options::HttpPost(formParts)); 
   std::cout << request << std::endl;
   return EXIT_SUCCESS;
--- simple_exchange_server.cc
+++ simple_exchange_server.cc
@@ -1,6 +1,8 @@
+#include <chrono>
 #include <httpserver.hpp>
 #include <string>
 
+#include <thread>
 #include <wolfssl/options.h>
 #include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/wolfcrypt/aes.h>
@@ -8,53 +10,66 @@
 
 #include "cmake/build/simple_exchange.pb.h"
 using namespace httpserver;
-using simpleexchange::SimpleExample;
+using simpleexchange::ResponseProto;
 
 typedef unsigned char byte;
 typedef unsigned int word32;
 
 const byte KEY[16] = "test";
 
+std::string decryptResponse(const ResponseProto response_proto) 
+{ 
+  //TODO get key
+  
+  
+  std::string cipher = std::string(response_proto.encrypted_payload());
+  //decryption
+  Aes dec; 
+
+  byte* input;
+  byte* output;
+
+  int i = 0;
+  int length = cipher.length();
+  int sz = length * sizeof(byte); 
+
+
+  input = (byte*) malloc(sz);
+  output = (byte*) malloc(sz);
+
+  std::memcpy((char *)input, cipher.c_str(), sz);
+  
+  if (wc_AesSetKey(&dec, KEY, AES_BLOCK_SIZE, NULL, AES_DECRYPTION ) != 0)
+    std::cout << "failed to set key\n";
+
+  if (wc_AesCbcDecrypt(&dec, output, input, sz ) != 0) 
+  { 
+    std::cout << "decryption failed\n";
+    return NULL;
+  }
+  
+  //getting rid of padding
+  length -= output[length-1];
+
+  std::cout << "decryption successful\n";
+  return std::string( reinterpret_cast< char const* >(output), length);
+} 
 
 class test_resource : public http_resource {
   public:
     
     std::shared_ptr<http_response> render(const http_request& req) {
       
-      std::string cipher = std::string(req.get_arg("protobuf")); 
+      std::string data = std::string(req.get_arg("protobuf")); 
+      std::cout << data << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+      ResponseProto response_proto; 
+      response_proto.ParseFromString(data);
+      std::cout << response_proto.DebugString() << std::endl; 
       
-      //decryption
-      Aes dec; 
-      
-      byte* input;
-      byte* output;
-
-      int i = 0;
-      int length = cipher.length();
-      int sz = length * sizeof(byte); 
-      
-      
-      input = (byte*) malloc(sz);
-      output = (byte*) malloc(sz);
-      
-      std::strcpy((char *)input, cipher.c_str());
-      
-      if (wc_AesSetKey(&dec, KEY, AES_BLOCK_SIZE, NULL, AES_DECRYPTION ) != 0)
-        std::cout << "failed to set key\n";
-      
-      if (wc_AesCbcDecrypt(&dec, output, input, sz ) != 0)
-        //std::cout << "decryption failed\n";
-      //else    
-        //std::cout <<"decryption successful\n";
-      ;
-      
-      //getting rid of padding
-      length -= output[length-1];
-      std::string decryptedData = std::string( reinterpret_cast< char const* >(output), length);
-
-      SimpleExample simple_example;
-      simple_example.ParseFromString(decryptedData);
-      return std::shared_ptr<http_response>(new string_response("Protobuf text: " + std::string(simple_example.text())));
+      std::string payload = decryptResponse(response_proto);
+      std::cout << "decrypted payload : " << payload <<std::endl;
+      return std::shared_ptr<http_response>(new string_response("Protobuf text: " + data));
     }
 };
 
